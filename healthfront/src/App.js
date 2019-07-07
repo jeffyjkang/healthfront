0460d99f7ae3f89368d85c887b98e451b68088c0
jeffyjkang <jeffyjkang@gmail.com>
2019-07-06 23:03:03 -0700

    basic login functionality

--- healthfront/src/App.js
+++ healthfront/src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import Authenticate from "./authentication/Authenticate";
+import Login from "./authentication/Login";
+import ComponentContainer from "./components/ComponentContainer";
+import { Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div>App</div>
-    </div>
-  );
+const AuthComponent = Authenticate(ComponentContainer)(Login);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Health Logger</h1>
+        <Route path="/" component={AuthComponent} />
+      </div>
+    );
+  }
 }
 
 export default App;
