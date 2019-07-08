78d96567fe026e86808e15976595babbf10ff3ea
jeffyjkang <jeffyjkang@gmail.com>
2019-07-07 22:48:27 -0700

    added calendar and signout to navbar

--- healthfront/src/components/ComponentContainer.js
+++ healthfront/src/components/ComponentContainer.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
+import NavBar from "./navbar/NavBar";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = () => ({
+  container: {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  }
+});
 
 class ComponentContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  signOut = e => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    this.props.logOut();
+    this.props.history.push("/login");
+  };
   render() {
+    const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.container}>
+        <NavBar signOut={this.signOut} />
         <div>App</div>
       </div>
     );
   }
 }
 
-export default ComponentContainer;
+export default withStyles(styles)(ComponentContainer);
