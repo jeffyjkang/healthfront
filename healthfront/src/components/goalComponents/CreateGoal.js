0d6ccbeebcc43e7a4682ea707207ca75c1520a32
jeffyjkang <jeffyjkang@gmail.com>
2019-07-09 23:17:56 -0700

    create goal component setup

---
+++ healthfront/src/components/goalComponents/CreateGoal.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Axios from "axios";
+//
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+const styles = () => ({});
+
+class CreateGoal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(CreateGoal);
