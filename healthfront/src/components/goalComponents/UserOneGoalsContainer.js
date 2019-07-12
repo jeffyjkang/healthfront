7f1020e0e55d3ce79c53c500ff5c44431b34164f
jeffyjkang <jeffyjkang@gmail.com>
2019-07-11 23:07:43 -0700

    map user goals

---
+++ healthfront/src/components/goalComponents/UserOneGoalsContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import UserOneGoal from "./UserOneGoal";
+
+const styles = () => ({
+  container: {
+    border: "1px solid blue"
+  }
+});
+
+const UserOneGoalsContainer = props => {
+  const { classes } = props;
+  return (
+    <div className={classes.container}>
+      {props.userOneGoals.map(userOneGoal => (
+        <UserOneGoal key={Math.random()} userOneGoal={userOneGoal} />
+      ))}
+    </div>
+  );
+};
+
+export default withStyles(styles)(UserOneGoalsContainer);
