5f8701b6aa96f202c679041fc70f0336adb7e9e9
jeffyjkang <jeffyjkang@gmail.com>
2019-07-17 00:27:43 -0700

    view day info update

--- healthfront/src/components/dayComponents/UserOneDay.js
+++ healthfront/src/components/dayComponents/UserOneDay.js
@@ -2,6 +2,8 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 //
 import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Popover from "@material-ui/core/Popover";
@@ -25,6 +27,9 @@ const styles = () => ({
     justifyContent: "space-around",
     padding: 2,
     minWidth: 106
+  },
+  card: {
+    border: "2px solid #3F51B5"
   }
 });
 
@@ -42,15 +47,47 @@ const UserOneDay = props => {
   function handleClose1() {
     setAnchorEl1(null);
   }
+  function handleClick2(e) {
+    setAnchorEl2(e.currentTarget);
+  }
+  function handleClose2() {
+    setAnchorEl2(null);
+  }
+  function handleClick3(e) {
+    setAnchorEl3(e.currentTarget);
+  }
+  function handleClose3() {
+    setAnchorEl3(null);
+  }
+  function handleClick4(e) {
+    setAnchorEl4(e.currentTarget);
+  }
+  function handleClose4() {
+    setAnchorEl4(null);
+  }
+  const open1 = Boolean(anchorEl1);
+  const open2 = Boolean(anchorEl2);
+  const open3 = Boolean(anchorEl3);
+  const open4 = Boolean(anchorEl4);
+
+  const id1 = open1 ? "simple-popper1" : undefined;
+  const id2 = open2 ? "simple-popper2" : undefined;
+  const id3 = open3 ? "simple-popper3" : undefined;
+  const id4 = open4 ? "simple-popper4" : undefined;
   console.log(props);
   return (
     <div>
-      <Card>
+      <Card className={classes.card}>
         <Typography variant="subtitle2">
           Date: {"  "} {props.userOneDay.dailyDate}
         </Typography>
         <Divider />
-        <Button className={classes.button} variant="outlined">
+        <Button
+          className={classes.button}
+          variant="outlined"
+          aria-describedby={id1}
+          onClick={handleClick1}
+        >
           <SearchIcon fontSize="small" />
           <Typography variant="caption">E.Notes</Typography>
           <Badge
@@ -60,8 +97,39 @@ const UserOneDay = props => {
             <FitnessIcon fontSize="small" />
           </Badge>
         </Button>
+        <Popover
+          id={id1}
+          open={open1}
+          anchorEl={anchorEl1}
+          onClose={handleClose1}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+          }}
+        >
+          <Card className={classes.card}>
+            <CardHeader title="Exercise Notes:" />
+            <Divider />
+            <CardContent>
+              <Typography>
+                {props.userOneDay.exerciseNotes
+                  ? props.userOneDay.exerciseNotes
+                  : "No notes have been added"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Popover>
         <Divider />
-        <Button className={classes.button} variant="outlined">
+        <Button
+          className={classes.button}
+          variant="outlined"
+          aria-describedby={id2}
+          onClick={handleClick2}
+        >
           <SearchIcon fontSize="small" />
           <Typography variant="caption">F.Notes</Typography>
           <Badge
@@ -71,8 +139,39 @@ const UserOneDay = props => {
             <RestaurantIcon fontSize="small" />
           </Badge>
         </Button>
+        <Popover
+          id={id2}
+          open={open2}
+          anchorEl={anchorEl2}
+          onClose={handleClose2}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+          }}
+        >
+          <Card className={classes.card}>
+            <CardHeader title="Food Notes:" />
+            <Divider />
+            <CardContent>
+              <Typography>
+                {props.userOneDay.foodNotes
+                  ? props.userOneDay.foodNotes
+                  : "No notes have been added"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Popover>
         <Divider />
-        <Button className={classes.button} variant="outlined">
+        <Button
+          className={classes.button}
+          variant="outlined"
+          aria-describedby={id3}
+          onClick={handleClick3}
+        >
           <SearchIcon fontSize="small" />
           <Typography variant="caption">S.Notes</Typography>
           <Badge
@@ -82,8 +181,39 @@ const UserOneDay = props => {
             <HotelIcon fontSize="small" />
           </Badge>
         </Button>
+        <Popover
+          id={id3}
+          open={open3}
+          anchorEl={anchorEl3}
+          onClose={handleClose3}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+          }}
+        >
+          <Card className={classes.card}>
+            <CardHeader title="Sleep Notes:" />
+            <Divider />
+            <CardContent>
+              <Typography>
+                {props.userOneDay.sleepNotes
+                  ? props.userOneDay.sleepNotes
+                  : "No notes have been added"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Popover>
         <Divider />
-        <Button className={classes.button} variant="outlined">
+        <Button
+          className={classes.button}
+          variant="outlined"
+          aria-describedby={id4}
+          onClick={handleClick4}
+        >
           <SearchIcon fontSize="small" />
           <Typography variant="caption">M.Notes</Typography>
           <Badge
@@ -93,6 +223,32 @@ const UserOneDay = props => {
             <CategoryIcon fontSize="small" />
           </Badge>
         </Button>
+        <Popover
+          id={id4}
+          open={open4}
+          anchorEl={anchorEl4}
+          onClose={handleClose4}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+          }}
+        >
+          <Card className={classes.card}>
+            <CardHeader title="Miscellaneous Notes:" />
+            <Divider />
+            <CardContent>
+              <Typography>
+                {props.userOneDay.miscNotes
+                  ? props.userOneDay.miscNotes
+                  : "No notes have been added"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Popover>
         <Divider />
         <Button className={classes.button} variant="outlined" color="primary">
           <Typography variant="caption">Update</Typography>
